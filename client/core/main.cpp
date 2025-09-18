8b6110b7b9af20635ef5e0143d4612708aec6415
xafbw <74664838+xafbw@users.noreply.github.com>
2025-09-18 21:24:57 +0800

    Update main.cpp

--- client/core/main.cpp
+++ client/core/main.cpp
@@ -25,12 +25,25 @@ int main(int argc, char *argv[])
     string addrs[3] = {"127.0.0.1:12345", "127.0.0.1:12346", "127.0.0.1:12347"};
     uint32_t windowSize = 256;
     uint32_t numBuckets = 256;
-    int depth = 4;
+    int depth = 1;
+
     vector<uint32_t> data(windowSize, 1);
+
     vector<shared_ptr<grpc::Channel>> channels;
-    for (int i = 0; i < NUM_SERVERS; i++)
-    {
-        shared_ptr<grpc::Channel> channel = grpc::CreateChannel(addrs[i], grpc::InsecureChannelCredentials());
+    for (int i = 0; i < NUM_SERVERS; i++) {
+
+
+        grpc::ChannelArguments args;
+        args.SetMaxReceiveMessageSize(1024 * 1024 * 1024); // grpc max 1024MB
+        args.SetMaxSendMessageSize(1024 * 1024 * 1024);    // grpc max 1024MB
+        
+        shared_ptr<grpc::Channel> channel = grpc::CreateCustomChannel(
+            addrs[i],
+            grpc::InsecureChannelCredentials(),
+            args
+        );
+
+        //shared_ptr<grpc::Channel> channel = grpc::CreateChannel(addrs[i], grpc::InsecureChannelCredentials());  grpc max 4MB
         channels.push_back(channel);
     }
     printf("going to create client\n");
